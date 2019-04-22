2556d9ccf5508a38f215714ec3a3f88f34506a57
mantvydas <razuleviciusmantvydas@gmail.com>
2019-04-22 21:41:05 +0300

    user object/repository created

--- src/Controller/MatchingController.php
+++ src/Controller/MatchingController.php
@@ -3,18 +3,28 @@
 namespace App\Controller;
 
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class MatchingController extends AbstractController
 {
     /**
      * @Route("/match", name="matching")
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $user = $this->getPossibleRoommates($em);
         return $this->render('matching/match.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'someVariable' => $user[1]->getValue(),
         ]);
+    }
+
+    private function getPossibleRoommates(EntityManagerInterface $em)
+    {
+        $repository = $em->getRepository(User::class);
+        $users = $repository->findAll();
+        return $users;
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
