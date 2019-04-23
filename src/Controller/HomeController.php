34a04d43b6eacb6c1ef00273e5b224a945c8d039
lukas balabis <l.balabonas@gmail.com>
2019-04-23 21:43:18 +0300

    Deleted commented code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,20 +13,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-
-        $entityManager = $this->getDoctrine()->getManager();
-//
-        $user = new User();
-        $user->setCityCode("KAUNAS");
-        $user->setDateOfBirth(new \DateTime());
-        $user->setEmail("test@testgmail.com");
-        $user->setFullName("Testas Testuotojas");
-        $user->setGender("Male");
-//
-        $entityManager->persist($user);
-//
-        $entityManager->flush();
-
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
         ]);
--- src/Repository/ProvidedAnswerRepository.php
+++ src/Repository/ProvidedAnswerRepository.php
@@ -18,33 +18,4 @@ class ProvidedAnswerRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ProvidedAnswer::class);
     }
-
-    // /**
-    //  * @return ProvidedAnswer[] Returns an array of ProvidedAnswer objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ProvidedAnswer
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
