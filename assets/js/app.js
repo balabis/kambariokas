94e8e589450b99e82da77a68d9ab8c6cd3215a63
mantvydas <razuleviciusmantvydas@gmail.com>
2019-04-23 15:03:58 +0300

    Hobbies list added to view.

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,6 @@ require('../css/app.scss');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 });
--- src/Controller/HobbiesController.php
+++ src/Controller/HobbiesController.php
@@ -3,18 +3,32 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Hobby;
 
 class HobbiesController extends AbstractController
 {
     /**
      * @Route("/hobbies", name="hobbies")
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $hobbies = $this->getAllHobbies($em);
         return $this->render('matching/hobbies.html.twig', [
-            'someVariable' => 'some hobby',
+            'hobbiesList' => $hobbies,
         ]);
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @return array
+     */
+    public function getAllHobbies(EntityManagerInterface $em) :array
+    {
+        $repository = $em->getRepository(Hobby::class);
+        $hobbies = $repository->findAll();
+        return $hobbies;
     }
 }
