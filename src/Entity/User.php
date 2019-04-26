db8dac7c1951631f38cad22c4c10aaec079633de
balabis <l.balabonas@gmail.com>
2019-04-26 21:40:15 +0300

    fixed blank line violation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
