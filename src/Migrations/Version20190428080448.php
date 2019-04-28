9a20a00aafb97ef7488069a14b82394f3c5a8419
Domantas Stankus <domantax@gmail.com>
2019-04-28 13:41:56 +0300

    updated data loading to db and migrations

--- src/Migrations/Version20190428080448.php
+++ src/Migrations/Version20190428080448.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190427154322 extends AbstractMigration
+final class Version20190428080448 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -26,7 +26,8 @@ final class Version20190427154322 extends AbstractMigration
         $this->addSql('CREATE TABLE question (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', question_text VARCHAR(255) NOT NULL, questionnaire_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', full_name VARCHAR(255) DEFAULT NULL, email VARCHAR(180) NOT NULL, gender VARCHAR(255) DEFAULT NULL, date_of_birth DATETIME DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_answer (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', question_id INT NOT NULL, question_answer_id INT NOT NULL, user_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE question_answers (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', question_id INT NOT NULL, answer_text VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question_answers (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', question_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', answer_text VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_5E0C131B1E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE question_answers ADD CONSTRAINT FK_5E0C131B1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
     }
 
     public function down(Schema $schema) : void
@@ -34,6 +35,7 @@ final class Version20190427154322 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE question_answers DROP FOREIGN KEY FK_5E0C131B1E27F6BF');
         $this->addSql('DROP TABLE questionnaire');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE user');
