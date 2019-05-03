b31f0043ebed930abbbf07084d9c085cb5cb4066
balabis <l.balabonas@gmail.com>
2019-05-03 23:04:02 +0300

    Updated UploadController to save profile picture in separate users dir

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -8,13 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Service\FileUploader;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UploadController extends AbstractController
 {
     /**
      * @Route("/flatmate/doUpload", name="upload")
      */
-    public function index(Request $request, string $uploadDir, FileUploader $uploader)
+    public function index(Request $request, string $uploadDir, FileUploader $uploader, UserInterface $user)
     {
         $token = $request->get("token");
 
@@ -22,15 +23,17 @@ class UploadController extends AbstractController
             return new Response("Operation not allowed", Response::HTTP_BAD_REQUEST, ['content-type' => 'text/plain']);
         }
 
-        $file = $request->files->get('myfile');
+        $file = $request->files->get('profile_picture');
+
         if (empty($file)) {
             return new Response("No file specified",
                 Response::HTTP_UNPROCESSABLE_ENTITY, ['content-type' => 'text/plain']);
         }
 
+        $userId = $user->getId()->toString();
         $filename = $file->getClientOriginalName();
-        $uploader->upload($uploadDir.'profile_pictures', $file, $filename);
+        $uploader->upload($uploadDir.'profile_pictures/'.$userId, $file, $filename);
 
-        return new Response("File uploaded", Response::HTTP_OK, ['content-type' => 'text/plain']);
+        return $this->render('profile/profileView.html.twig');
     }
 }
