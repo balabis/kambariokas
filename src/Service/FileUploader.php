bbc641b713b6a4f35abcf22a51c0423ada2438a6
balabis <l.balabonas@gmail.com>
2019-05-04 12:52:16 +0300

    Created FileUploader service

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -5,15 +5,34 @@ namespace App\Service;
 
 
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileUploader
 {
-    public function upload($uploadDir, $file, $filename)
+
+    private $targetDirectory;
+
+    public function __construct($targetDirectory)
     {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function uploadProfilePicture(UploadedFile $file, string $id)
+    {
+        $filename = md5(uniqid()) . '.' . $file->guessExtension();
+        $uploadDir = $this->getTargetDirectory() . 'profile_pictures/' . $id;
+
         try {
             $file->move($uploadDir, $filename);
         } catch (FileException $e) {
             throw new FileException('Failed to upload file');
         }
+
+        return $filename;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
     }
 }
