1a2f06b3147b7addac2ba65740b3ff6afe7f4bd3
mantvydas <razuleviciusmantvydas@gmail.com>
2019-05-05 22:02:15 +0300

    Match entity create

--- src/Entity/Match.php
+++ src/Entity/Match.php
@@ -4,5 +4,76 @@ namespace App\Entity;
 
 class Match
 {
+    private $id;
+
+    private $firstUser;
+
+    private $secondUser;
+
+    private $coefficient;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstUser()
+    {
+        return $this->firstUser;
+    }
+
+    /**
+     * @param mixed $firstUser
+     */
+    public function setFirstUser($firstUser): void
+    {
+        $this->firstUser = $firstUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecondUser()
+    {
+        return $this->secondUser;
+    }
+
+    /**
+     * @param mixed $secondUser
+     */
+    public function setSecondUser($secondUser): void
+    {
+        $this->secondUser = $secondUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCoefficient()
+    {
+        return $this->coefficient;
+    }
+
+    /**
+     * @param mixed $coefficient
+     */
+    public function setCoefficient($coefficient): void
+    {
+        $this->coefficient = $coefficient;
+    }
 
 }
