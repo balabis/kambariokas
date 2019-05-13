407280bac424427115db65fae090c705a6aed2fe
Domantas Stankus <domantax@gmail.com>
2019-05-14 01:23:07 +0300

    refactored matchController
    to pass only one var to template

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -2,15 +2,11 @@
 
 namespace App\Controller;
 
+
 use App\Entity\User;
-use App\Services\PaginationService;
-use Doctrine\ORM\EntityManagerInterface;
-use Pagerfanta\Pagerfanta;
-use Pagerfanta\View\TwitterBootstrap4View;
+use App\Services\MatchesPaginationService;
 use App\Services\MatchService;
-use App\Services\UserCompareService;
 use App\Services\UserService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,30 +16,24 @@ class MatchController extends AbstractController
     /**
      * @Route("/matched", name="matched")
      */
-    public function index(PaginationService $ps, Request $request)
-    {
-        $pagerfanta = $ps->getPagerfanta();
-        $pagerfanta->setCurrentPage($request->query->get('page', 1));
-
     public function index(
-        EntityManagerInterface $entityManager,
         MatchService $service,
-        UserService $userService,
-        UserCompareService $compareService
+        MatchesPaginationService $ps,
+        Request $request
     ) {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $service->filter($this->getUser());
 
         $matches = $service->getPossibleMatch($this->getUser());
-        $usersName = $userService->getAllUsersNamesByUUID($matches);
+        $pagerfanta = $ps->getPagerfanta($matches);
+        $pagerfanta->setMaxPerPage(8);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
 
         return $this->render('match/index.html.twig', [
             'pagerfanta' => $pagerfanta,
             'contentName' => 'Match',
             'matchesInfo' => $matches,
-            'usersName' => $usersName,
-            'userCount'=> (count($matches) - 1)
         ]);
     }
 }
