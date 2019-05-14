564f2ffd2150898d3aceb9c3acb13390e6a34cfb
Domantas Stankus <domantax@gmail.com>
2019-05-14 15:11:23 +0300

    changed pagination service to use DoctrineORMAdapter

--- src/Services/MatchesPaginationService.php
+++ src/Services/MatchesPaginationService.php
@@ -5,22 +5,40 @@ namespace App\Services;
 
 
 use Doctrine\ORM\EntityManagerInterface;
-use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 
 class MatchesPaginationService
 {
+    private $em;
 
-    private function getAdapter($array)
+    public function __construct(EntityManagerInterface $em)
     {
-        $adapter = new ArrayAdapter($array);
+        $this->em = $em;
+    }
+
+    private function createQueryBuilderToGetMatchUsers($userId)
+    {
+        $queryBuilder = $this->em->createQueryBuilder()
+            ->select('um')
+            ->from('App\Entity\UserMatch', 'um')
+            ->andWhere('um.firstUser = :userId')
+            ->setParameter('userId', $userId)
+            ->orderBy('um.coeficient', 'DESC');
+
+        return $queryBuilder;
+    }
+
+    private function getAdapter($userId)
+    {
+        $queryBuilder = $this->createQueryBuilderToGetMatchUsers($userId);
+        $adapter = new DoctrineORMAdapter($queryBuilder);
         return $adapter;
     }
 
-    public function getPagerfanta($array)
+    public function getPagerfanta($userId)
     {
-        $adapter = $this->getAdapter($array);
+        $adapter = $this->getAdapter($userId);
         $pagerfanta = new Pagerfanta($adapter);
         return $pagerfanta;
     }
