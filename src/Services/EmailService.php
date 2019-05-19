0fe7b887e1d724c9b065dac7cd8f9d4f4f06a125
Domantas Stankus <domantax@gmail.com>
2019-05-19 16:39:19 +0300

    updated emailService

--- src/Services/EmailService.php
+++ src/Services/EmailService.php
@@ -3,37 +3,97 @@
 
 namespace App\Services;
 
-
 use Twig\Environment;
-
 
 class EmailService
 {
     private $mailer;
     private $twigEnvironment;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twigEnvironment )
-    {
+    public function __construct(
+        \Swift_Mailer $mailer,
+        Environment $twigEnvironment
+    ) {
         $this->mailer = $mailer;
         $this->twigEnvironment = $twigEnvironment;
     }
 
-    public function sentInvitationEmail($email, $senderUser) {
-        $message = (new \Swift_Message('Kvietimas tapti kambariokais'))
+    private function sentEmail($email, $firstParagraph, $secondParagraph, $subject)
+    {
+        $message = (new \Swift_Message($subject))
             ->setFrom('geriausiaskambariokas@gmail.com')
             ->setTo($email)
             ->setBody(
                 $this->twigEnvironment->render(
                     'emails/invitation.html.twig',
                     [
-                        'senderName' => $senderUser->getFullName(),
-                        'uuid'=>$senderUser->getId()
+                        'firstParagraph' => $firstParagraph,
+                        'secondParagraph'=>$secondParagraph
                     ]
                 ),
                 'text/html'
             );
 
-
         $this->mailer->send($message);
+    }
+
+    public function sentInvitationEmail($email, $senderUser)
+    {
+        $firstParagraph =
+            'Informuojame, kad rumis.io vartotojas ' .
+            $senderUser->getFullName() . ' išsiuntė Jums kvietimą tapti kambariokais.';
+
+        $secondParagraph =
+            'Norėdami peržiūrėti ir patvirtinti arba atmesti 
+            kvietimą spauskite mygtuką apačioje. Linkime geros dienos!';
+
+        $subject = 'Kvietimas tapti kambariokais';
+
+        $this->sentEmail($email, $firstParagraph, $secondParagraph, $subject);
+    }
+
+    public function sentInviteCancelEmail($email, $senderUser)
+    {
+        $firstParagraph =
+            'Informuojame, kad rumis.io vartotojas ' .
+            $senderUser->getFullName() . ' atšaukė Jums išsiųstą kvietimą tapti kambariokais.';
+
+        $secondParagraph =
+            'Norėdami peržiūrėti Jūsų išsiųstus ar gautus pakvietimus spauskite mygtuką apačioje. 
+            Linkime geros dienos!';
+
+        $subject = 'Atšauktas kvietimas tapti kambariokais';
+
+        $this->sentEmail($email, $firstParagraph, $secondParagraph, $subject);
+    }
+
+    public function sentAcceptInvitationEmail($email, $senderUser)
+    {
+        $firstParagraph =
+            'Informuojame, kad rumis.io vartotojas ' .
+            $senderUser->getFullName() . ' priėmė Jūsų išsiųstą kvietimą tapti kambariokais.';
+
+        $secondParagraph =
+            'Norėdami gauti daugiau informacijos spauskite mygtuką apačioje. 
+            Linkime geros dienos!';
+
+        $subject = 'Priimtas kvietimas tapti kambariokais';
+
+        $this->sentEmail($email, $firstParagraph, $secondParagraph, $subject);
+    }
+
+    public function sentDeclineInvitationEmail($email, $senderUser)
+    {
+        $firstParagraph =
+            'Informuojame, kad rumis.io vartotojas ' .
+            $senderUser->getFullName() . ' atmetė Jūsų išsiųstą kvietimą tapti kambariokais.';
+
+        $secondParagraph =
+            'Norėdami gauti daugiau informacijos spauskite mygtuką apačioje. 
+            Linkime geros dienos!';
+
+        $subject = 'Atmestas kvietimas tapti kambariokais';
+
+        $this->sentEmail($email, $firstParagraph, $secondParagraph, $subject);
     }
 }
