123403c1d80f8e7ca348bf07292f723dbcf527ca
mantvydas <razuleviciusmantvydas@gmail.com>
2019-05-22 15:59:48 +0300

    Created form

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\MatchFilterFormType;
 use App\Services\MatchesPaginationService;
 use App\Services\MatchService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormTypeInterface;
 
 class MatchController extends AbstractController
 {
@@ -25,9 +27,13 @@ class MatchController extends AbstractController
         $matchesPagination->setMaxPerPage(8);
         $matchesPagination->setCurrentPage($request->query->get('page', 1));
 
+        $form = $this->createForm(MatchFilterFormType::class);
+        $form->handleRequest($request);
+
         return $this->render('match/index.html.twig', [
             'matches' => $matchesPagination,
             'contentName' => 'Match',
+            'filterForm' => $form->createView()
         ]);
     }
 }
--- src/Form/MatchFilterFormType.php
+++ src/Form/MatchFilterFormType.php
@@ -2,9 +2,23 @@
 
 namespace App\Form;
 
+use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
 
 class MatchFilterFormType extends AbstractType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('gender', ChoiceType::class, [
+              'choices' => [
+                  'Prefer not to say' => 'default',
+                  'Male' => 'male',
+                  'Female' => 'female',
+                  ],
+              ]);
+    }
 }
