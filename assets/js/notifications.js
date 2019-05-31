5caee7ef00ee12e5d2706557a8a2af16aa197986
Domantas Stankus <domantax@gmail.com>
2019-05-31 12:37:03 +0300

    ajax call and front-end finished

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,40 +1,55 @@
 var $ = require('jquery');
 
-const ajaxCall = function () {
+ajaxCall();
+setInterval(ajaxCall, 20000);
+
+function ajaxCall() {
     var notificationRoute = $('#notifications').data('notification_route');
 
-    var deleteNotificationRoute = $('#notifications').data('delete_notification_route');
-    console.log(notificationRoute, deleteNotificationRoute);
     $.post(notificationRoute, function(response){
+        if (response.length === 0) {
+            if (document.getElementById('notification-bell').classList.contains('text-danger')) {
+                document.getElementById('notification-bell').classList.remove('text-danger');
+            }
+            if (document.getElementById('no-notifications').classList.contains('d-none')) {
+                document.getElementById('no-notifications').classList.remove('d-none');
+            }
+        } else {
+            if (! document.getElementById('notification-bell').classList.contains('text-danger')) {
+                document.getElementById('notification-bell').classList.add('text-danger');
+            }
+            if (! document.getElementById('no-notifications').classList.contains('d-none')) {
+                document.getElementById('no-notifications').classList.add('d-none');
+            }
+        }
+
         response.forEach(notification => {
-            console.log(notification);
-
-            var form = document.createElement("form");
-            form.setAttribute('method',"post");
-            form.setAttribute('action', deleteNotificationRoute );
-
-            var input = document.createElement("input"); //input element, text
-            input.setAttribute('type',"hidden");
-            input.setAttribute('name',"notificationId");
-            input.setAttribute('value', notification.notification.id );
-
-            var button = document.createElement("button"); //input element, Submit button
-            button.setAttribute('type',"submit");
-            button.setAttribute('class',"dropdown-item");
-
-            var buttonText = document.createTextNode(notification.notification.subject + ' - ' + notification.notification.message);
-            button.appendChild(buttonText);
-            form.appendChild(input);
-            form.appendChild(button);
-
-            document.getElementById('notification-list').innerHTML = '';
-            document.getElementById('notification-list').appendChild(form);
+            createNotificationForm(notification);
         })
     })
-};
+}
 
-setInterval(ajaxCall, 5000);
+function createNotificationForm (notification) {
+    var deleteNotificationRoute = $('#notifications').data('delete_notification_route');
 
+    var form = document.createElement("form");
+    form.setAttribute('method',"post");
+    form.setAttribute('action', deleteNotificationRoute );
 
+    var input = document.createElement("input");
+    input.setAttribute('type',"hidden");
+    input.setAttribute('name',"notificationId");
+    input.setAttribute('value', notification.notification.id );
 
+    var button = document.createElement("button");
+    button.setAttribute('type',"submit");
+    button.setAttribute('class',"dropdown-item");
 
+    var buttonText = document.createTextNode(notification.notification.subject + ' - ' + notification.notification.message);
+    button.appendChild(buttonText);
+    form.appendChild(input);
+    form.appendChild(button);
+
+    document.getElementById('notification-list').innerHTML = '';
+    document.getElementById('notification-list').appendChild(form);
+}
--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -4,10 +4,6 @@
 namespace App\Controller;
 
 
-use Doctrine\ORM\EntityManagerInterface;
-use Mgilet\NotificationBundle\Entity\NotifiableEntity;
-use Mgilet\NotificationBundle\Entity\NotifiableNotification;
-use Mgilet\NotificationBundle\Entity\Notification;
 use Mgilet\NotificationBundle\Manager\NotificationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,21 +14,13 @@ class NotificationsController extends AbstractController
     /**
      * @Route("/notification/delete", name="delete_notification", methods={"POST"})
      */
-    public function deleteNotification(Request $request, NotificationManager $manager, EntityManagerInterface $em)
+    public function deleteNotification(Request $request, NotificationManager $manager)
     {
-
         $notifId = $request->request->get('notificationId');
         $notification = $manager->getNotification($notifId);
-//        $notifiableNotificationRepo = $em->getRepository(NotifiableNotification::class);
-//        $notifiableNotification = $notifiableNotificationRepo->findBy(['id'=>$notifId]);
-
-//        $notifiableEntityRepo = $em->getRepository(NotifiableEntity::class);
-//        $notifiableEntityRepo->removeNotifiableNotification($notifiableNotification);
         $manager->removeNotification(array($this->getUser()), $notification, true);
         $manager->deleteNotification($notification, true);
-//        $notification = $notificationRepo->findOneBy(['id'=>$notifId]);
-//        $manager->r
-//
+
         return $this->redirect($notification->getLink());
     }
 
